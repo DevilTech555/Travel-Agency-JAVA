740d37e48f94292cdd159dc02f2d4c1af8cdbe50
DevilTech555 <pmnaveen6@gmail.com>
2021-03-21 21:01:12 +0530

    removed all wrong version

--- Travel-Agency/src/com/saarathi/controller/CustomerController.java
+++ Travel-Agency/src/com/saarathi/controller/CustomerController.java
@@ -1,21 +1,10 @@
 package com.saarathi.controller;
 
-import java.sql.SQLException;
 import java.util.Scanner;
-
-import com.saarathi.beans.TripPlanner;
-import com.saarathi.service.LoginValidationService;
-import com.saarathi.service.RouteTripService;
-import com.saarathi.service.TourPackageService;
-import com.saarathi.service.TripPlannerService;
 
 public class CustomerController {
 
-	public void menuList() throws SQLException {
-		TripPlannerService tripPlannerService = new TripPlannerService();
-		TourPackageService tourPackageService = new TourPackageService();
-		RouteTripService routeTripService = new RouteTripService();
-		LoginValidationService loginValidationService = new LoginValidationService();
+	public void menuList(){
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("enter email:");
@@ -34,16 +23,8 @@ public class CustomerController {
 			
 			switch(option) {
 			case 1:
-				tourPackageService.showTourPackages();
-//				System.out.println("enter tour package id to book:");
-//				int tourPackageId = scanner.nextInt();
-//				System.out.println("1.Tour Package");
-//				System.out.println("2.Route Trip");
-//				System.out.println("3.View booked Status");
-//				System.out.println("4.cancellation");
 				break;
 			case 2:
-				routeTripService.showRouteTrip();
 				break;
 			}
 		}while(option!=0);
